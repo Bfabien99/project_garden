2647a7eae76fea0213bfe56125d86ccb8da184c4
FabienBrou <fabienbrou99@gmail.com>
2025-01-23 14:12:02 +0000

    add all routes

---
+++ app/main.py
@@ -0,0 +1,16 @@
+import uvicorn
+from fastapi import FastAPI
+from database import create_database
+from routes import organization, project, fake_route
+app = FastAPI()
+
+@app.on_event('startup')
+def initiate():
+    create_database()
+
+app.include_router(organization.router)
+app.include_router(project.router)
+app.include_router(fake_route.router)
+
+if __name__ == "__main__":
+    uvicorn.run(app=app, host="0.0.0.0", port=8000)
